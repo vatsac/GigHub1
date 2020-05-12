750dec849445260fcf75e4d94b634b726d155998
Aditya Kumar <avats218@gmail.com>
2020-05-12 11:34:58 +0530

    Creating an get notification api

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,56 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    
+    
+    public class NotificationsController : ApiController
+    {
+        private DbEntities _context;
+        public NotificationsController()
+        {
+            _context = new DbEntities();
+        }
+        
+        [HttpGet]
+        [Authorize]
+        IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.AspNetUser)
+                .ToList();
+            return notifications.Select(n => new NotificationDto()
+            {
+                DateTime = n.DateTime,
+                Gig = new GigDto
+                {
+                    Artist = new UserDto
+                    {
+                        Id = n.Gig.Artist_Id,
+                        Name = n.Gig.AspNetUser.Name
+                    },
+                    DateTime = n.Gig.DateTime,
+                    ID = n.Gig.ID,
+                    IsCanceled = n.Gig.IsCanceled,
+                    Venue = n.Gig.Venue
+                },
+                OriginalDateTime = n.OriginalDateTime,
+                OriginalVenue = n.OriginalVenue,
+                Type=(NotificationType)n.Type
+
+            });
+
+        }
+
+    }
+}
